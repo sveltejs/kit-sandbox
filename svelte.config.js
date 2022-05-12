fe1373e44aae5bac4c916b56725bdd2779aa9457
Rich Harris <hello@rich-harris.dev>
2022-05-12 14:54:35 -0400

    use netlify adapter for now

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			edge: true
+		}),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
