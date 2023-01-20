3f8ea1b8ea1387dd699551be42e355ca8fe32b17
Rich Harris <hello@rich-harris.dev>
2023-01-19 20:17:46 -0500

    declare global

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {}
+declare global {
+	namespace App {}
+}
+
+export {};
