0d0660eb4911402f02a7a3d9726e5bf0ea290e6a
Rich Harris <git@rich-harris.dev>
2023-05-17 14:03:38 -0400

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,7 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		vite: {
-			server: {
-				fs: {
-					allow: [path.resolve('../kit/packages/kit/src')]
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
